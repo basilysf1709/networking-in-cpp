f993ac2b163d7790e7a771e29ea74c9a50559b76
Basil Yusuf <basilyusuf1709@gmail.com>
2023-09-13 18:49:53 +0300

    Adding comments

--- server.cpp
+++ server.cpp
@@ -8,22 +8,25 @@ int main() {
     /* This line creates a socket. AF_INET specifies that this is an IPv4 socket, 
         and SOCK_STREAM indicates that this socket will be used for TCP/IP communication. */
     int serverSocket = socket(AF_INET, SOCK_STREAM, 0);
-    
+
     if (serverSocket == -1) {
         std::cerr << "Error creating socket" << std::endl;
         return -1;
     }
 
+    // Setting up Server Address
     sockaddr_in serverAddress;
     serverAddress.sin_family = AF_INET;
     serverAddress.sin_port = htons(12345); // Port number
     serverAddress.sin_addr.s_addr = INADDR_ANY; // Listen on all available interfaces
 
+    // Binding the socket
     if (bind(serverSocket, (struct sockaddr*)&serverAddress, sizeof(serverAddress)) == -1) {
         std::cerr << "Error binding socket" << std::endl;
         return -1;
     }
 
+    // Listening for connections
     if (listen(serverSocket, 5) == -1) {
         std::cerr << "Error listening on socket" << std::endl;
         return -1;
@@ -34,16 +37,19 @@ int main() {
     sockaddr_in clientAddress;
     socklen_t clientAddrSize = sizeof(clientAddress);
 
+    // Accepting a connection
     int clientSocket = accept(serverSocket, (struct sockaddr*)&clientAddress, &clientAddrSize);
     if (clientSocket == -1) {
         std::cerr << "Error accepting connection" << std::endl;
         return -1;
     }
 
+    // Receiving Data from Client
     char buffer[1024] = {0};
     recv(clientSocket, buffer, sizeof(buffer), 0);
     std::cout << "Received: " << buffer << std::endl;
 
+    // Closing Client and Server Sockets
     close(clientSocket);
     close(serverSocket);
 
